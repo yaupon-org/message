e75bd060396072ed2b7fe82f30fd724c5e93fc84
Jakub Olan <keinsell@protonmail.com>
2024-06-15 10:38:44 +0200

    chore: ✉️ update `@yaupon/message`

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { ID, id } from "@yaupon/id";
-import {MergeDeep} from "type-fest";
-import {Ark, define, Scope, Type, type as arktype,} from 'arktype'
-import deepmerge from "deepmerge";
-import {DefinitionParser, validateTypeRoot} from "arktype/out/type";
 
 // Copied from: https://github.com/keinsell/escentia/blob/d6864388a850b1a964a1dd44eb31eb099b9ead7c/src/eips/messages/message.ts#L23
-export enum MessageKind {
+export enum MessageType {
   /**
    * @see [Enterprise Integration Patterns](https://www.enterpriseintegrationpatterns.com/patterns/messaging/EventMessage.html)
    */
@@ -52,53 +48,95 @@ export interface MessageHeaders {
   readonly causationId?: ID | string | null;
 }
 
+export type CreateNewMessageIntent<P = unknown> = {
+  messageType: MessageType;
+  messageName: string;
+};
+
 export type CreateMessageIntent<P = unknown> = {
   id?: ID;
-  messageType: MessageKind;
+  messageType: MessageType;
+  messageName: string;
   headers: MessageHeaders | null;
   payload: P;
   timestamp?: Date;
   metadata: Record<string, unknown> | null;
 };
 
-type IMessageBodySchema = Ark
+export class MessageIntent<PAYLOAD = unknown> {
+  readonly id!: ID;
+  readonly messageType!: MessageType;
+  /** Dot-case name of the message
+  @example "user.created""
+  */
+  readonly messageName!: string;
+  readonly timestamp!: Date;
+  readonly headers!: MessageHeaders | null;
+  readonly payload!: PAYLOAD;
+  readonly metadata!: Record<string, unknown> | null;
 
-interface IMessageHeaders {
-  [key: string]: string;
-}
+  constructor(create_message: {
+    id?: ID;
+    messageType: MessageType;
+    messageName: string;
+    headers: MessageHeaders | null;
+    payload: PAYLOAD;
+    timestamp?: Date;
+    metadata: Record<string, unknown> | null;
+  }) {
+    this.id = id({ type: create_message.messageType.toLowerCase() });
+    this.messageType = create_message.messageType;
+    this.messageName = create_message.messageName;
+    this.timestamp = create_message.timestamp ?? new Date();
+    this.headers = create_message.headers ?? null;
+    this.payload = create_message.payload;
+    this.metadata = create_message.metadata ?? null;
+  }
 
-/**
- * Comparing to {@link IMessageHeaders} metadata is not serialized,
- * and it's there just to add additional information to class, this
- * type is extendable as there may be more fields than provided
- * by default.
- */
-type IMessageMetadata<AdditionalData extends Record<string, any> = {}> = MergeDeep<{ id: ID, dateCreated: Date }, AdditionalData>
-
-
-class Message<Body extends Record<string, any> = {}> {
-  public id: ID
-  private type: string;
-  private kind: MessageKind;
-  private readonly schemaDefinition?: any;
-  private headers?: IMessageHeaders;
-  private metadata: IMessageMetadata;
-  public body?: Body
-  public timestamp: number = Date.now()
-
-  constructor(type: string, kind: MessageKind, schema: validateTypeRoot<Body, Ark>, headers?: IMessageHeaders, metadata?: Partial<IMessageMetadata>) {
-    this.type = type;
-    this.kind = kind;
-    this.id = id({type: kind.toLowerCase()}) as any
-    this.headers = headers;
-    this.metadata = deepmerge({id: this.id, dateCreated: new Date()} satisfies IMessageMetadata, metadata ?? {})
-
-    this.schemaDefinition = schema
+  clone(): MessageIntent<PAYLOAD> {
+    return structuredClone(this);
   }
 }
 
-const CreateUserCommand: Message<{username: string}> = new Message("create.user", MessageKind.COMMAND, {
-      username: "string"
-})
+export interface Message<BODY = unknown> extends MessageIntent<BODY> {
+  readonly id: ID;
+  readonly messageType: MessageType;
+  readonly messageName: string;
+  readonly timestamp: Date;
+  readonly headers: MessageHeaders | null;
+  readonly payload: BODY;
+  readonly metadata: Record<string, unknown> | null;
+  new(body: BODY): Message<BODY>;
+}
 
-console.log(CreateUserCommand)
+
+
+export function defineMessage<P>(
+  defineNewMessageIntent: CreateNewMessageIntent<P>,
+): Message<P> {
+  return class extends MessageIntent<P> {
+    constructor(create_message: P, headers?: MessageHeaders, metadata?: Record<string, unknown>) {
+      super(
+        {
+          messageName: defineNewMessageIntent.messageName,
+          messageType: defineNewMessageIntent.messageType,
+          payload: create_message,
+          headers: headers ? headers : null,
+          metadata: metadata ? metadata : null,
+        },
+      );
+    };
+
+    static get messageName(): string {
+      return defineNewMessageIntent.messageName;
+    }
+  } as any as Message<P>;
+}
+
+
+const UserCreated = defineMessage<{ username: string }>({
+  messageType: MessageType.EVENT,
+  messageName: "user.created",
+});
+
+console.log(new UserCreated({ username: "keinsell" }))
